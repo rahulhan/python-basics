ab0ce384f8c98cbe3970d4b8b48e09cb71a481f1
Rahul Handay <rahulha@saltstack.com>
2015-05-23 22:09:45 +0530

    Adding user details model

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Details
 
-# Register your models here.
+class DetailsAdmin(admin.ModelAdmin):
+
+    """
+        ModelAdmin for user Details
+    """
+    list_display = ['user', 'father_name', 'mother_name', 'city']
+
+admin.site.register(Details, DetailsAdmin)
