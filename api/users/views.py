5515d3f43691b36efd31fa93805c259c3b9c998d
Rahul Handay <rahulha@saltstack.com>
2015-05-24 22:40:58 +0530

    Adding user details view

--- api/users/views.py
+++ api/users/views.py
@@ -12,11 +12,9 @@ logger = logging.getLogger("api.users")
 
 
 class AddUser(generics.ListCreateAPIView):
-
     """
         View to create and add a user
     """
-
     def post(self, request):
         """
             Parameters:
@@ -48,5 +46,53 @@ class AddUser(generics.ListCreateAPIView):
             response_dict = dict(username=self.username)
             return Response(dict(error=[], data=response_dict), status=status.HTTP_200_OK)
         except:
-            return Response(dict(error=["user cant not be added"], data=dict()),
+            return Response(dict(error=["user_cant_not_be_added"], data=dict()),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddDetails(generics.ListCreateAPIView):
+    """
+        View to add user details
+    """
+    def post(self, request):
+        """
+            Parameters:
+            {
+                "father_name": "father's name",
+                "mother_name": "mother's name",
+                "city": "city",
+                "user": "username" 
+            }
+        """
+        self.username = request.DATA.get("user", None)
+        self.father = request.DATA.get("father_name", None)
+        self.mother = request.DATA.get("mother_name", None)
+        self.city = request.DATA.get("city", None)
+
+        if not self.username:
+            return Response(dict(error=["username_empty"], data=dict()),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            self.userObj = User.objects.get(username=self.username)
+        except:
+            self.userObj = None
+
+        if not self.userObj:
+            return Response(dict(error=["user_does_not_exists"], data=dict()),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            self.detailObj = Details.objects.create(father_name=self.father,
+                                                    mother_name=self.mother,
+                                                    city=self.city,
+                                                    user=self.userObj)
+            self.detailObj.save()
+            response_dict = dict(father_name=self.father,
+                                 mother_name=self.mother,
+                                 city=self.city,
+                                 user=self.username)
+            return Response(dict(error=[], data=response_dict), status=status.HTTP_200_OK)
+        except:
+            return Response(dict(error=["details_cant_not_be_saved"], data=dict()),
                             status=status.HTTP_400_BAD_REQUEST)
