7b5d2bcc46b8559095371efa3a8fe2efeca3fb39
Rahul Handay <rahulha@saltstack.com>
2015-05-25 00:02:48 +0530

    User model fix

--- api/users/models.py
+++ api/users/models.py
@@ -9,9 +9,8 @@ class Details(models.Model):
     father_name = models.CharField(max_length=255)
     mother_name = models.CharField(max_length=255, null=True)
     city = models.CharField(max_length=50)
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
 
     class Meta:
-        unique_together = (("user"),)
         ordering = ['user']
         verbose_name_plural = "Details"
