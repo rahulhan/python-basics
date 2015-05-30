d5484a8122808375a3962568244c41c5a57870a1
Rahul Handay <rahulha@saltstack.com>
2015-05-30 16:58:53 +0530

    Adding user delete unit tests

--- api/users/tests/crud_user_tests.py
+++ api/users/tests/crud_user_tests.py
@@ -35,7 +35,6 @@ class UserAddTest(TestCase):
         """
             Test of empty username as input
         """
-        print("hi")
         data = '{"username": "", "password": "test"}'
         response = self.c.post(
             "/users/add/",
@@ -61,6 +60,70 @@ class UserAddTest(TestCase):
         data = '{"username": "testuser1", "password": "test"}'
         response = self.c.post(
             '/users/add/',
+            data, content_type="application/json",
+            **{"HTTP_AUTHORIZATION": "Token testuser"})
+        self.assertEqual(response.status_code, 200)
+
+
+class UserDeleteTest(TestCase):
+    """
+        Usage: python manage.py test users.tests
+    """
+    def setUp(self):
+        """
+            Create test objects
+        """
+        self.c = Client()
+        self.user_obj = User.objects.create_user(
+            "testuser",
+            "testuser@test.com", "testuser"
+        )
+        self.user_obj1 = User.objects.create_user(
+            "testuser1",
+            "testuser1@test.com", "testuser1"
+        )
+        self.token = Token.objects.create(key="testuser", user=self.user_obj)
+
+        self.global_config = GlobalConfig.objects.create(
+            name="token_exp",
+            value="60"
+        )
+
+    def tearDown(self):
+        """
+            Destroy test objects
+        """
+        User.objects.all().delete()
+
+    def test_empty_username(self):
+        """
+            Test of empty username as input
+        """
+        data = '{"user": ""}'
+        response = self.c.delete(
+            "/users/delete/",
+            data, content_type="application/json",
+            **{"HTTP_AUTHORIZATION": "Token testuser"})
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_user(self):
+        """
+            Test to delete invalid user
+        """
+        data = '{"user": "test"}'
+        response = self.c.delete(
+            "/users/delete/",
+            data, content_type="application/json",
+            **{"HTTP_AUTHORIZATION": "Token testuser"})
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_user(self):
+        """
+            Test to delete user
+        """
+        data = '{"user": "testuser1"}'
+        response = self.c.delete(
+            "/users/delete/",
             data, content_type="application/json",
             **{"HTTP_AUTHORIZATION": "Token testuser"})
         self.assertEqual(response.status_code, 200)
